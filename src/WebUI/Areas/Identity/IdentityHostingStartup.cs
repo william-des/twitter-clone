c53622f472930b3bf6428b029065fc0f35e9abe0
wdesgardin <williamdesgardin@gmail.com>
2021-04-03 00:17:06 +0200

    chore: scaffold identity login and register UI

---
+++ src/WebUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TwitterClone.Infrastructure.Identity;
+using TwitterClone.Infrastructure.Persistence;
+
+[assembly: HostingStartup(typeof(TwitterClone.WebUI.Areas.Identity.IdentityHostingStartup))]
+namespace TwitterClone.WebUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
