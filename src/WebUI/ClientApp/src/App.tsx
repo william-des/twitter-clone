fdab98b01e842620fa725c381b38be7a11868778
wdesgardin <williamdesgardin@gmail.com>
2021-05-11 20:56:14 +0200

    chore: remove counter

--- src/WebUI/ClientApp/src/App.tsx
+++ src/WebUI/ClientApp/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { Counter } from "./components/Counter";
 import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
 import ApiAuthorizationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
 import { ApplicationPaths } from "./components/api-authorization/ApiAuthorizationConstants";
@@ -16,7 +15,6 @@ export const App: React.FC = () => {
 			<Route path="/">
 				<Layout>
 					<Route exact path="/" component={Feed} />
-					<Route path="/counter" component={Counter} />
 					<Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
 				</Layout>
 			</Route>
