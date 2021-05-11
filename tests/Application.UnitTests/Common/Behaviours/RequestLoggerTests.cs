86b838e9dbc38d971f5549d67a474c6a0a219160
wdesgardin <williamdesgardin@gmail.com>
2021-05-11 21:47:19 +0200

    test: reactivate request logger tests

--- tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -5,45 +5,46 @@ using Moq;
 using NUnit.Framework;
 using System.Threading;
 using System.Threading.Tasks;
+using TwitterClone.Application.Posts.Commands.CreatePost;
 
 namespace TwitterClone.Application.UnitTests.Common.Behaviours
 {
     public class RequestLoggerTests
     {
-        // private readonly Mock<ILogger<CreateTodoItemCommand>> _logger;
-        // private readonly Mock<ICurrentUserService> _currentUserService;
-        // private readonly Mock<IIdentityService> _identityService;
+        private readonly Mock<ILogger<CreatePostCommand>> _logger;
+        private readonly Mock<ICurrentUserService> _currentUserService;
+        private readonly Mock<IIdentityService> _identityService;
 
 
-        // public RequestLoggerTests()
-        // {
-        //     _logger = new Mock<ILogger<CreateTodoItemCommand>>();
+        public RequestLoggerTests()
+        {
+            _logger = new Mock<ILogger<CreatePostCommand>>();
 
-        //     _currentUserService = new Mock<ICurrentUserService>();
+            _currentUserService = new Mock<ICurrentUserService>();
 
-        //     _identityService = new Mock<IIdentityService>();
-        // }
+            _identityService = new Mock<IIdentityService>();
+        }
 
-        // [Test]
-        // public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
-        // {
-        //     _currentUserService.Setup(x => x.UserId).Returns("Administrator");
+        [Test]
+        public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
+        {
+            _currentUserService.Setup(x => x.UserId).Returns("Administrator");
 
-        //     var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+            var requestLogger = new LoggingBehaviour<CreatePostCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
 
-        //     await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+            await requestLogger.Process(new CreatePostCommand { Content="To the moon !" }, new CancellationToken());
 
-        //     _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
-        // }
+            _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
+        }
 
-        // [Test]
-        // public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
-        // {
-        //     var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+        [Test]
+        public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
+        {
+            var requestLogger = new LoggingBehaviour<CreatePostCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
 
-        //     await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+            await requestLogger.Process(new CreatePostCommand { Content="To the moon !" }, new CancellationToken());
 
-        //     _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
-        // }
+            _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
+        }
     }
 }
