431f5095642057ed056792af51d405c3c1d9b247
wdesgardin <williamdesgardin@gmail.com>
2021-05-17 18:30:57 +0200

    feat: load follows on init

--- src/WebUI/ClientApp/src/App.tsx
+++ src/WebUI/ClientApp/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router";
 import AuthorizeRoute from "./auth/AuthorizeRoute";
 import ApiAuthorizationRoutes from "./auth/ApiAuthorizationRoutes";
@@ -6,10 +6,34 @@ import { ApplicationPaths } from "./auth/ApiAuthorizationConstants";
 import { Layout } from "./layout/Layout";
 import { Register } from "./auth/Register";
 import Feed from "./feed/Feed";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import store from "./core/Store";
 import "./styles.css";
 import UserProfile from "./user/UserProfile";
+import authService from "./auth/AuthorizeService";
+import { FollowsClient } from "./core/WebApiClient";
+import { setFollows } from "./user/FollowsActions";
+
+const ApplicationRoutes: React.FC = (props) => {
+	const dispatch = useDispatch();
+	const loadFollows = async () => {
+		let follows = undefined;
+		const domainUser = await authService.getDomainUser();
+		if (!!domainUser) {
+			follows = await new FollowsClient().getUserFollows(domainUser.id);
+		}
+		dispatch(setFollows(follows));
+	};
+	useEffect(() => {
+		loadFollows();
+		const subscription = authService.subscribe(() => loadFollows());
+		return () => {
+			authService.unsubscribe(subscription);
+		};
+	});
+	
+	return <React.Fragment>{props.children}</React.Fragment>;
+};
 
 export const App: React.FC = () => {
 	return (
@@ -18,10 +42,12 @@ export const App: React.FC = () => {
 				<AuthorizeRoute path="/create-account" component={Register} />
 				<Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
 				<Route path="/">
-					<Layout>
-						<Route exact path="/" component={Feed} />
-						<Route path="/:username" component={UserProfile} />
-					</Layout>
+					<ApplicationRoutes>
+						<Layout>
+							<Route exact path="/" component={Feed} />
+							<Route path="/:username" component={UserProfile} />
+						</Layout>
+					</ApplicationRoutes>
 				</Route>
 			</Switch>
 		</Provider>
