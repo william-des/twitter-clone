a86530b6f4cf2f71ca271242f11b1dc5efc33f75
wdesgardin <williamdesgardin@gmail.com>
2021-05-18 21:45:31 +0200

    refactor(post-card): extract button component

--- src/WebUI/ClientApp/src/feed/PostCard.tsx
+++ src/WebUI/ClientApp/src/feed/PostCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { IPostDto } from "../core/WebApiClient";
 import ReactTimeAgo from "react-time-ago";
 import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faRetweet, faShareSquare } from "@fortawesome/free-solid-svg-icons";
+import { faRetweet, faShareSquare, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LinkParser from "./LinkParser";
 import UserPicture from "../user/UserPicture";
@@ -11,9 +11,23 @@ import { NavLink } from "react-router-dom";
 const PostCard: React.FC<IPostDto> = (props) => {
 	const randomInt = () => Math.ceil(Math.random() * 100);
 
+	const Button = (btnProps: { icon: IconDefinition; color: string }) => {
+		return (
+			<button className={`flex-grow group flex items-center focus:outline-none hover:text-${btnProps.color}`}>
+				<div
+					className={`mr-1 group-hover:bg-opacity-10 rounded-full w-8 h-8 items-center justify-center flex group-hover:bg-${btnProps.color}`}
+				>
+					<FontAwesomeIcon icon={btnProps.icon} className="mx-auto" />
+				</div>
+				<span className="text-sm">{randomInt()}</span>
+			</button>
+		);
+	};
+
 	return (
-		<article className="border-b p-3 flex">
-			<UserPicture pictureId={props?.createdBy?.pictureId} className="h-12 w-12 mr-4" />
+		<article className="border-b p-3">
+			<div className="flex">
+				<UserPicture pictureId={props?.createdBy?.pictureId} className="h-12 w-12 mr-3 mt-1" />
 			<div className="flex flex-1 flex-col">
 				<div className="flex">
 					<h2 className="font-semibold mr-1 hover:underline">
@@ -29,22 +43,12 @@ const PostCard: React.FC<IPostDto> = (props) => {
 				{!!props.mediaId && (
 					<img src={`api/medias/${props.mediaId}`} className="border border-gray-300 rounded-xl my-2" />
 				)}
-				<div className="flex justify-between w-5/6 text-gray-500 font-light">
-					<button className="w-16 text-left">
-						<FontAwesomeIcon icon={faComment} className="mr-2" />
-						{randomInt()}
-					</button>
-					<button className="w-16 text-left">
-						<FontAwesomeIcon icon={faRetweet} className="mr-2" />
-						{randomInt()}
-					</button>
-					<button className="w-16 text-left">
-						<FontAwesomeIcon icon={faHeart} className="mr-2" />
-						{randomInt()}
-					</button>
-					<button className="w-16 text-left">
-						<FontAwesomeIcon icon={faShareSquare} className="mr-2" />
-					</button>
+					<div className="flex text-gray-500 font-light w-full text-md">
+						<Button icon={faComment} color="primary" />
+						<Button icon={faRetweet} color="green-400" />
+						<Button icon={faHeart} color="pink-500" />
+						<Button icon={faShareSquare} color="primary" />
+					</div>
 				</div>
 			</div>
 		</article>
