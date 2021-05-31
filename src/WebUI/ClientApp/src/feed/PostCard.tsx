f883acaa45ffafd0825cbf6d70a8294aaf077bf3
wdesgardin <williamdesgardin@gmail.com>
2021-05-31 18:23:30 +0200

    fix: nested anchor link

--- src/WebUI/ClientApp/src/feed/PostCard.tsx
+++ src/WebUI/ClientApp/src/feed/PostCard.tsx
@@ -3,17 +3,20 @@ import { IPostDto } from "../core/WebApiClient";
 import Post from "./Post";
 import FollowerActivity from "./FollowerActivity";
 import PostButtonRow from "./PostButtonRow";
-import { NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const PostCard: React.FC<IPostDto> = (props) => {
+	const history = useHistory();
+
 	return (
-		<article className="border-b p-3 hover:bg-gray-50 transition-colors cursor-pointer">
-			<NavLink to={`/status/${props.id}`}>
-				<FollowerActivity {...props} />
-				<Post post={props}>
-					<PostButtonRow post={props}/>
-				</Post>
-			</NavLink>
+		<article
+			className="border-b p-3 hover:bg-gray-50 transition-colors cursor-pointer"
+			onClick={() => history.push(`/status/${props.id}`)}
+		>
+			<FollowerActivity {...props} />
+			<Post post={props}>
+				<PostButtonRow post={props} />
+			</Post>
 		</article>
 	);
 };
