df1b77cf59c956329d57ba0ad0d84b1e9ca425fb
wdesgardin <williamdesgardin@gmail.com>
2021-06-09 22:28:52 +0200

    feat: notification icon by type

--- src/WebUI/ClientApp/src/notifcations/NotificationItem.tsx
+++ src/WebUI/ClientApp/src/notifcations/NotificationItem.tsx
@@ -7,6 +7,36 @@ import { markAsRead } from "../core/actions/NotificationsActions";
 import { INotificationDto, NotificationsClient, NotificationType } from "../core/WebApiClient";
 import Card from "../shared/Card";
 import UserPicture from "../user/UserPicture";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faRetweet } from "@fortawesome/free-solid-svg-icons";
+
+const notificationsConfig = {
+	[NotificationType.Follow]: {
+		label: "followed you",
+		icon: faTwitter,
+		iconColor: "primary",
+	},
+	[NotificationType.Mention]: {
+		label: "mentioned you in a tweet",
+		icon: faComment,
+		iconColor: "primary",
+	},
+	[NotificationType.Answer]: {
+		label: "replied to your tweet",
+		icon: faComment,
+		iconColor: "primary",
+	},
+	[NotificationType.RePost]: {
+		label: "retweeted your tweet",
+		icon: faRetweet,
+		iconColor: "green-400",
+	},
+	[NotificationType.Like]: {
+		label: "liked your tweet",
+		icon: faHeart,
+		iconColor: "pink-500",
+	},
+};
 
 const NotificationItem: React.FC<INotificationDto> = (props) => {
 	const dispatch = useDispatch();
@@ -23,31 +53,15 @@ const NotificationItem: React.FC<INotificationDto> = (props) => {
 		);
 	};
 
-	const getLabel = (type: NotificationType) => {
-		switch (type) {
-			case NotificationType.Follow:
-				return "followed you";
-			case NotificationType.Answer:
-				return "replied to your tweet";
-			case NotificationType.Mention:
-				return "mentioned you in a tweet";
-			case NotificationType.RePost:
-				return "retweeted your tweet";
-			case NotificationType.Like:
-				return "liked your tweet";
-			default:
-				return "";
-		}
-	};
-
 	return (
 		<Card onClick={onClick} className={`py-3 px-5 ${props.read ? "" : "bg-primary bg-opacity-5"}`}>
 			<div className="flex">
-				<FontAwesomeIcon className="text-primary text-3xl mr-2" icon={faTwitter} />
+				<FontAwesomeIcon className={`text-2xl mr-2 text-${notificationsConfig[props.type].iconColor}`} icon={notificationsConfig[props.type].icon} />
 				<div className="flex flex-col">
 					<UserPicture className="h-8 w-8" pictureId={props?.createdBy?.pictureId} />
 					<div className="mt-1">
-						<span className="font-bold">{props.createdBy.fullName}</span> {getLabel(props.type)}
+						<span className="font-bold">{props.createdBy.fullName}</span>{" "}
+						{notificationsConfig[props.type].label}
 					</div>
 					<div className="mt-1 text-gray-500">{props.postContent}</div>
 				</div>
